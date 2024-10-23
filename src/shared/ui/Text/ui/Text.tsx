3bc0a559e969cd38994f39319290fde2f370c73a
Ivanna Liuba <i.it.bulka@gmail.com>
2024-10-23 13:01:49 +0200

    add sizes for text

--- src/shared/ui/Text/ui/Text.tsx
+++ src/shared/ui/Text/ui/Text.tsx
@@ -13,12 +13,18 @@ export enum TextAlign {
   CENTER = 'center',
 }
 
+export enum TextSize {
+  M = 'size_m',
+  L = 'size_l',
+}
+
 interface TextProps {
   className?: string
   title?: string
   text?: string
   theme?: TextTheme
   align?: TextAlign
+  size?: TextSize
 }
 
 export const Text = memo(({
@@ -26,12 +32,13 @@ export const Text = memo(({
   text,
   title,
   theme= TextTheme.PRIMARY,
-  align = TextAlign.LEFT
+  align = TextAlign.LEFT,
+  size = TextSize.M
 }: TextProps) => {
   return (
     <div
       data-testid="text"
-      className={classnames(cls.text, {}, [className, cls[theme], cls[align]])}
+      className={classnames(cls.text, {}, [className, cls[theme], cls[align], cls[size]])}
     >
       {title && <p className={cls.title}>{title}</p>}
       {text && <p className={cls.text}>{text}</p>}
