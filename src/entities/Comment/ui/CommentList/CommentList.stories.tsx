beb79091cb76d69eb22fa6d267e69d23d7f2d671
Ivanna Liuba <i.it.bulka@gmail.com>
2024-10-26 09:51:37 +0200

    fix stories

--- src/entities/Comment/ui/CommentList/CommentList.stories.tsx
+++ src/entities/Comment/ui/CommentList/CommentList.stories.tsx
@@ -1,9 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { Comment } from 'entities/Comment'
 import { CommentList } from './CommentList'
 
+const comments: Comment[] = [
+  {
+    id: '1',
+    text: 'hello world',
+    user: { id: '1', username: 'Vasya' }
+  },
+  {
+    id: '2',
+    text: 'Comment 2',
+    user: { id: '1', username: 'Petya' }
+  }
+]
+
 const meta = {
-  title: 'shared/CommentList',
-  component: CommentList
+  title: 'entities/CommentList',
+  component: CommentList,
+  args: {
+    comments
+  }
 } satisfies Meta<typeof CommentList>
 
 export default meta
--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
@@ -1,12 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator'
 import AddCommentForm from './AddCommentForm'
 
 const meta = {
-  title: 'shared/AddCommentForm',
+  title: 'feature/AddCommentForm',
   args: {
     onSendComment: action('onSendComment')
   },
+  decorators: [StoreDecorator({})],
   component: AddCommentForm
 } satisfies Meta<typeof AddCommentForm>
 
