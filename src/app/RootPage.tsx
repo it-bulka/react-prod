c5b4721506c3122781317f3952d6657da1edd470
Ivanna Liuba <i.it.bulka@gmail.com>
2024-10-26 10:23:33 +0200

    fix storing data of authorized user

--- src/app/RootPage.tsx
+++ src/app/RootPage.tsx
@@ -1,10 +1,18 @@
-import { FC } from 'react'
+import { FC , useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import cls from 'shared/libs/classnames/classnames'
 import { NavBar } from 'widgets/NavBar/index'
 import { SideBar } from 'widgets/SideBar/ui/SideBar'
+import { userActions } from 'entities/User/model/slice/userSlice'
+import { useAppDispatch } from 'app/providers/StoreProvider/config/store'
 
 export const RootPage: FC = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(userActions.initAuthData())
+  }, [])
+
   return (
     <div className={cls('app')}>
       <NavBar />
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -3,6 +3,10 @@ import { useTranslation } from 'react-i18next'
 import { LoginModal } from 'features/AuthByUsername'
 import { useState, memo, useCallback } from 'react'
 import { Button, ThemeButton } from 'shared/ui/Button/Button'
+import { getUserAuthData } from 'entities/User'
+import { useSelector } from 'react-redux'
+import { useAppDispatch } from 'app/providers/StoreProvider/config/store'
+import { userActions } from 'entities/User/model/slice/userSlice'
 import cls from './NavBar.module.scss'
 
 interface NavBarProps {
@@ -12,10 +16,26 @@ interface NavBarProps {
 export const NavBar = memo(({ className }: NavBarProps) => {
   const { t } = useTranslation()
   const [isAuthModalOpen, setIsAuthModalOpen] = useState(false)
+  const authData = useSelector(getUserAuthData)
+  const dispatch = useAppDispatch()
 
   const openModal = useCallback(() => setIsAuthModalOpen(true), [setIsAuthModalOpen])
   const closeModal = useCallback(() => setIsAuthModalOpen(false),[setIsAuthModalOpen])
+  const onLogout = useCallback(() => dispatch(userActions.logout()), [dispatch])
 
+  if(authData) {
+    return (
+      <nav className={classnames(cls.navbar, {}, [className])}>
+        <Button
+          theme={ThemeButton.CLEAR_INVERTED}
+          className={cls.links}
+          onClick={onLogout}
+        >
+          {t('log_out')}
+        </Button>
+      </nav>
+    )
+  }
   return (
     <nav className={classnames(cls.navbar, {}, [className])}>
       <Button
