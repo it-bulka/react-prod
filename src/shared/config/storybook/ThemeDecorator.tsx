d769767db76edeecaa35c3dc33f3a5c5ffcf53aa
Ivanna Liuba <i.it.bulka@gmail.com>
2024-11-10 20:06:16 +0100

    fix using portals in storybook (preview-body.html); and theming for them.

--- src/shared/config/storybook/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator.tsx
@@ -2,11 +2,15 @@ import { Decorator } from '@storybook/react'
 import { Theme } from 'app/providers/lib/ThemeContext'
 import { DecoratorFactory } from 'shared/config/storybook/types'
 
-const ThemeDecoratorFactory: DecoratorFactory<Theme> = theme => Story => (
-  <div className={`app ${theme}`}>
-    <Story />
-  </div>
-)
+const ThemeDecoratorFactory: DecoratorFactory<Theme> = theme => Story => {
+  document.body.className = theme
+
+  return (
+    <div className="app">
+      <Story />
+    </div>
+  )
+}
 
 export const ThemeDecorator: Decorator = (Story, context) => {
   return ThemeDecoratorFactory(context.globals.theme)(Story, context)
