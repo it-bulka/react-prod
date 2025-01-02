ac549990f54ee0152b4d9aa6e407b842aad32d21
Ivanna Liuba <i.it.bulka@gmail.com>
2025-01-02 12:19:09 +0100

    add helpers for dispatch and selectors

---
+++ src/shared/libs/store/buildSlice.ts
@@ -0,0 +1,24 @@
+import {
+ createSlice, bindActionCreators, CreateSliceOptions, SliceCaseReducers
+} from '@reduxjs/toolkit'
+import { useMemo } from 'react'
+import { useDispatch } from 'react-redux'
+
+export function buildSlice<
+  State,
+  CaseReducers extends SliceCaseReducers<State>,
+  Name extends string = string
+>(options: CreateSliceOptions<State, CaseReducers, Name>) {
+  const slice = createSlice(options)
+
+  const useActions = () => {
+    const dispatch = useDispatch()
+
+    return useMemo(() => bindActionCreators(slice.actions, dispatch), [dispatch])
+  }
+
+  return {
+    ...slice,
+    useActions
+  }
+}
