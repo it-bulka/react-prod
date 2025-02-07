129ef75bf0990a5f069813beae5fcc6e591398f7
IT.bulka <i.it.bulka@gmail.com>
2025-02-07 10:18:07 +0100

    fix background for AppLayoutLoader

--- src/shared/lauouts/AppLoaderLayout/AppLoaderLayout.tsx
+++ src/shared/lauouts/AppLoaderLayout/AppLoaderLayout.tsx
@@ -9,23 +9,25 @@ import { MainLayout } from '../MainLayout'
 
 export const AppLoaderLayout = memo(() => {
   return (
-    <MainLayout
-      header={(
-        <HStack className={cls.header}>
-          <Skeleton width={40} height={40} border="50%" />
-        </HStack>
-      )}
-      content={(
-        <VStack gap="16" style={{ height: '100%' }}>
-          <Skeleton width="70%" height={32} border="16px" />
-          <Skeleton width="40%" height={20} border="16px" />
-          <Skeleton width="50%" height={20} border="16px" />
-          <Skeleton width="30%" height={32} border="16px" />
-          <Skeleton width="80%" height="40%" border="16px" />
-          <Skeleton width="80%" height="40%" border="16px" />
-        </VStack>
-      )}
-      sidebar={<Skeleton border="32px" width={220} height="100%" />}
-    />
+    <div className="app_redesigned">
+      <MainLayout
+        header={(
+          <HStack className={cls.header}>
+            <Skeleton width={40} height={40} border="50%" />
+          </HStack>
+        )}
+        content={(
+          <VStack gap="16" style={{ height: '100%' }}>
+            <Skeleton width="70%" height={32} border="16px" />
+            <Skeleton width="40%" height={20} border="16px" />
+            <Skeleton width="50%" height={20} border="16px" />
+            <Skeleton width="30%" height={32} border="16px" />
+            <Skeleton width="80%" height="40%" border="16px" />
+            <Skeleton width="80%" height="40%" border="16px" />
+          </VStack>
+        )}
+        sidebar={<Skeleton border="32px" width={220} height="100%" />}
+      />
+    </div>
   )
 })
